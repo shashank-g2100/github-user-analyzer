82e8977a37314a29635d7fcce13b37f9e8d7c11e
shashank-g2100 <97581841+shashank-g2100@users.noreply.github.com>
2025-05-23 19:29:03 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
+  title: 'Github-User-Analyzer',
   description: 'Created with v0',
   generator: 'v0.dev',
 }
